e0601266eecb81311e81f3394f9a9bda95bbb48d
Seiji Kumagai <seiji.kumagai@gmail.com>
2014-10-24 17:43:31 -0400

    implement core simulation

--- src/simulations.jl
+++ src/simulations.jl
@@ -1,31 +1,69 @@
 using SexSims
+using JSON
 
 function simulation()
-    srand(1234)
+    params = JSON.parsefile(ARGS[1])
+    println(params)
+    srand(1)
     tmax = 10
     nf = (100, 200)
     nm = (200, 100)
-    fb = (0.2, 0.3)
-    mb = (0.3, 0.2)
+    # Proportion of reproducing migrants out of all reproducing adults.
+    # This is sex- and deme-specific.
+    fb = [0.1, 0.3]
+    mb = [0.2, 0.1]
+    # cost of migration.  This is sex- and deme-specific
+    ft = [0.2, 0.3]
+    mt = [0.4, 0.1]
+    # Actual proportion of reproducing migrants.  This quantity takes cost into account.
+    # This is deme-specific and sex-specific in parents.
+    fv = tuple(fb .* ft ./ (fb .* ft + 1 - fb)...)
+    mv = tuple(mb .* mt ./ (mb .* mt + 1 - mb)...)
+    # Probability of mutation per locus per generation.  This is common across loci.
     mut = 0.02
-    fsel = (0.1, 0.2)
-    msel = (0.2, 0.2)
+    # Fitness of individuals carrying non-local trait (relative to local individuals)
+    # This is deme specific (in deme 1, in deme 2)
+    ffit = (0.2, 0.3)
+    mfit = (0.8, 0.9)
+    # Probabilities of learning parental trait.
+    # This is sex-specific in both parents and offspring as well as deme-specific.
+    f2fl = (0.1, 0.2)
+    m2fl = (0.2, 0.3)
+    f2ml = (0.4, 0.2)
+    m2ml = (0.2, 0.1)
 
     parents = Population(nf, nm)
     children = Population(nf, nm)
     rec = GeneStateRecorder(4 * (sum(nf) + sum(nm)))
 
     # Index of parents
-    pids = (ParentIds{Female}(nf), ParentIds{Male}(nm))
+    fpids = ParentIds(nf)
+    mpids = ParentIds(nm)
 
     for t = 1:tmax
-        parents, offspring = offspring, parents
+        parents, children = children, parents
+        fitness!(parents[Female], ffit...)
+        fitness!(parents[Male], mfit...)
         # mating
-        pickparents!(pids, parents, fb, mb)
-        reproduce!(t, children, parents, pids, mut, rec)
-        fitness!(children, fsel, msel)
+        # Select aprents of female offspring
+        pickparents!(nf, fpids, parents, fv, mv)
+        # Select parents of male offspring
+        pickparents!(nm, mpids, parents, fv, mv)
+        # Generating offspring
+        reproduce!(t, children, parents, (fpids, mpids), mut, rec)
         # learning
-        learn!(children, parents, pids, ftof, mtof, ftom, mtom)
+        learn!(children[Female], parents, fpids, f2fl, m2fl)
+        learn!(children[Male], parents, mpids, f2ml, m2ml)
     end
-    rec
+    rec, children
 end
+
+function summarize(rec, pop)
+end
+
+function main()
+    length(ARGS) != 1 && error("Usage: julia simulation.jl inputfile")
+    simulation()
+end
+
+main()
